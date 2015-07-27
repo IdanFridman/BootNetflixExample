784e4c44930ba0c606826a56ff33f0e47f0ce85b
idan <idan.frid@gmail.com>
2015-07-27 10:03:10 +0300

    Added amqp to aggregate hystrix results from all servers into one dashboard

--- core/flight-booking-service/src/main/java/com.bootnetflix.flight_booking_service/main/FlightBookingApplication.java
+++ core/flight-booking-service/src/main/java/com.bootnetflix.flight_booking_service/main/FlightBookingApplication.java
@@ -1,19 +1,18 @@
 package com.bootnetflix.flight_booking_service.main;
 
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by idan on 5/18/15.
  */
-@Configuration
+@SpringBootApplication //annotation is equivalent to using @Configuration, @EnableAutoConfiguration and @ComponentScan
 @ComponentScan("com.bootnetflix")
 @EnableDiscoveryClient
 public class FlightBookingApplication {
-
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(FlightBookingApplication.class).run(args);
